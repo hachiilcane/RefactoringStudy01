f864cdf121b90964111c7cd8143b7146e256b76d
hachiilcane <hachi.il.cane@gmail.com>
2015-06-03 19:58:33 +0900

    一時変数の削除

--- src/Refactoring01/Customer.java
+++ src/Refactoring01/Customer.java
@@ -33,12 +33,33 @@ class Customer {
 			// この貸出に関する数値の表示
 			result += "\t" + each.getMovie().getTitle() + "\t" +
 					String.valueOf(each.getCharge()) + "\n";
-			totalAmount += each.getCharge();
 		}
 		// フッタ部分の追加
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) +
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
 				" frequent renter points";
 		return result;
 	}
+
+	private double getTotalCharge() {
+		double result = 0;
+		Enumeration rentals = _rentals.elements();
+		while (rentals.hasMoreElements()){
+			Rental each = (Rental)rentals.nextElement();
+			result += each.getCharge();
+		}
+		return result;
+	}
+
+	private int getTotalFrequentRenterPoints(){
+		int result = 0;
+		Enumeration rentals = _rentals.elements();
+		while (rentals.hasMoreElements()){
+			Rental each = (Rental)rentals.nextElement();
+			result += each.getFrequentRenterPoints();
+		}
+		return result;
+
+	}
+
 }
