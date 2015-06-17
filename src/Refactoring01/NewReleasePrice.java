aa1f9b580a592e3590c1af498ee076d20b1be213
hachiilcane <hachi.il.cane@gmail.com>
2015-06-17 20:05:27 +0900

    料金計算の条件文をポリモーフィズムに置き換える

---
+++ src/Refactoring01/NewReleasePrice.java
@@ -0,0 +1,10 @@
+package Refactoring01;
+
+class NewReleasePrice extends Price {
+	int getPriceCode() {
+		return Movie.NEW_RELEASE;
+	}
+	double getCharge(int daysRented){
+		return daysRented * 3;
+	}
+}
---
+++ src/Refactoring01/Price.java
@@ -0,0 +1,7 @@
+package Refactoring01;
+
+abstract class Price {
+	abstract int getPriceCode();
+
+	abstract double getCharge(int daysRented);
+}
